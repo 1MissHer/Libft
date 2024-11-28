4829f8699f8e175c277b1aa6a8202f72654643ca
Lilian Lopez <lillopez@c2r2p2.42perpignan.fr>
2024-11-28 14:57:47 +0100

    libft

--- ft_strtrim/ft_strtrim.c
+++ ft_strtrim/ft_strtrim.c
@@ -6,39 +6,43 @@
 /*   By: lillopez <lillopez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/23 14:49:03 by lillopez          #+#    #+#             */
-/*   Updated: 2024/11/23 15:02:56 by lillopez         ###   ########.fr       */
+/*   Updated: 2024/11/28 14:54:07 by lillopez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 #include "libft.h"
 
-int ft_charset(char *charset, char c)
-{
-	int i;
-
-	i = 0;
-	while (charset[i])
-	{
-		if (charset[i] == c)
-			return (0);
-		i++;
-	}
-	return (1);
-}
-
 char *ft_strtrim(char const *s1, char const *set)
 {
 	int i;
 	int j;
+	int k;
+	int l;
 	char *str;
 
 	i = 0;
-	while (s1[i])
+	j = 0;
+	k = 0;
+	l = ft_strlen(s1);
+	str = s1;
+	while (set[j])
 	{
-		if (ft_charset(set, s1[i]) == 0)
-			return (str);
-		i++;
+		if (set[j] == str[i])
+		{
+			while (set[j] == str[i])
+				i++;
+		}
+		j++;
 	}
-	return (0);
+	while (set[k])
+	{
+		if (set[k] == str[l])
+		{
+			while (set[k] == str[l])
+				l--;
+		}
+		k++;
+	}
+	return (str);
 }
